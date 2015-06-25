2088a89075be00b89e771a82da24b96aafc3ab59
Fred Lundén <fred@softresource.se>
2015-06-25 17:30:06 +0200

    changed RouteConfig

--- SoftResource.DeployerServices/Services/App_Start/RouteConfig.cs
+++ SoftResource.DeployerServices/Services/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace DeployerServices
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Main", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Test", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
